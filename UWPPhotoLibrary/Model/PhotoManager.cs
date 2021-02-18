a14eefa92a8091c4b614d91e7179f1df4cbef862
LalithaNadimpalli <lalli.nadimpalli@gmail.com>
2021-02-18 13:46:27 -0800

    PhotoManager class changes

--- UWPPhotoLibrary/Model/PhotoManager.cs
+++ UWPPhotoLibrary/Model/PhotoManager.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace UWPPhotoLibrary.Model
 {
-    class PhotoManager
+    public static class PhotoManager
     {
+        private static List<Photo> getPhotos()
+        {
+            var photos = new List<Photo>();
+
+            photos.Add(new Photo("Animals1", AlbumName.Animals));
+            photos.Add(new Photo("Animals2", AlbumName.Animals));
+
+            photos.Add(new Photo("Baby1", AlbumName.Babies));
+            photos.Add(new Photo("Baby2", AlbumName.Babies));
+
+            photos.Add(new Photo("Flowers1", AlbumName.Flowers));
+            photos.Add(new Photo("Flowers2", AlbumName.Flowers));
+
+            photos.Add(new Photo("Fruits1", AlbumName.Fruits));
+            photos.Add(new Photo("Fruits2", AlbumName.Fruits));
+
+            photos.Add(new Photo("Nature", AlbumName.Nature));
+
+            return photos;
+
+
+        }
 
     }
 }
