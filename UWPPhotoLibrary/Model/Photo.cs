f2af9e8d0893e67d79ec5ccbd54664b9964de774
sushmakoosukuntla <78382542+sushmakoosukuntla@users.noreply.github.com>
2021-02-17 16:24:04 -0800

    Initial Photo library commit

---
+++ UWPPhotoLibrary/Model/Photo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPPhotoLibrary.Model
+{
+    public enum AlbumName
+    {
+        Animals,
+        Babies,
+        Flowers,
+        Fruits,
+        Nature
+    }
+    public class Photo
+    {
+        public AlbumName Album { get; set; }
+        public string ImageFile { get; set; }
+        public DateTime DateCreated { get; }
+        public string Name { get; set; }
+
+        //Below is the constructor
+        public Photo(AlbumName album)
+        {
+            ImageFile = $"/Assets/Audio/{Album}/{Name}.jpg";
+            Album = album;
+
+            /*I'm not defining the DateCreated, because we have to use 
+            .Now property when ever we add the photo in to the album*/
+        }
+    }
+}
