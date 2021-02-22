8d60183451782f430ac02aa783ba006cb317e076
sushmakoosukuntla <78382542+sushmakoosukuntla@users.noreply.github.com>
2021-02-22 12:06:09 -0800

    Photo library changes
    Photo Library

--- UWPPhotoLibrary/Model/PhotoManager.cs
+++ UWPPhotoLibrary/Model/PhotoManager.cs
@@ -24,6 +24,29 @@ namespace UWPPhotoLibrary.Model
             //allPhotos.ForEach(photo => photos.Add(photo));
         }
 
+        public static ObservableCollection<Album> GetAllAlbums()
+        {
+            //Empty list of type photo will get created in the line below and the list name is photos.
+            var albums = new ObservableCollection<Album>();
+            var allAlbums = GetAlbums();
+            foreach (var album in allAlbums)
+            {
+                albums.Add(album);
+            }
+            return albums;
+            //The above foreach can write like below(shortcut)
+            //allPhotos.ForEach(photo => photos.Add(photo));
+        }
+
+
+        public static void GetPhotosByCategory(ObservableCollection<Photo> photos, AlbumName category)
+        {
+            var allPhotos = GetPhotos();
+            var filteredPhotos = allPhotos.Where(photo => photo.AlbumName == category).ToList();
+            photos.Clear();
+            filteredPhotos.ForEach(photo => photos.Add(photo));
+        }
+
 
         /*We will be using below GetPhotos method for many other methods in this class, 
         that is the reason we are making it private and not using the type ObservableCollection*/
@@ -49,6 +72,19 @@ namespace UWPPhotoLibrary.Model
             photos.Add(new Photo("Nature", AlbumName.Nature));            
 
             return photos;
+        }
+
+
+        private static List<Album> GetAlbums()
+        {
+            var albums = new List<Album>();
+            albums.Add(new Album { AlbumType = AlbumName.Animals});
+            albums.Add(new Album { AlbumType = AlbumName.Babies });
+            albums.Add(new Album { AlbumType = AlbumName.Flowers });
+            albums.Add(new Album { AlbumType = AlbumName.Fruits });
+            albums.Add(new Album { AlbumType = AlbumName.Nature });
+
+            return albums;
         }
     }
 }
